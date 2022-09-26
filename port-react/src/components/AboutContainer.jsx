3507725d06324452e4492191e2da41c2eac4d356
Mateusnasciment <mateus.nasciment@hotmail.com>
2022-09-26 18:36:34 +0000

    first commit

---
+++ port-react/src/components/AboutContainer.jsx
@@ -0,0 +1,16 @@
+const AboutContainer = () => {
+    return (
+      <section className="about-container">
+        <h2>Sobre</h2>
+        <p>
+          Me chamo Mateus tenho 23 anos, sou um estudante em tecnologia !
+        </p>
+        <p> Estou me esforçando para me tornar um participante significativo na indústria de software.
+         Fora da aula, estou interessado em aprender e pesquisar por conta própria. Fui mergulhado em centenas de cursos on-line em profundidade e expandi meu conhecimento.
+        No par de tempo, construí muitos projetos para aprimorar minhas habilidades e compreensão das línguas e das ferramentas </p>
+        
+      </section>
+    );
+  };
+  
+  export default AboutContainer;
---
+++ port-react/src/components/ProjectsContainer.jsx
@@ -0,0 +1,15 @@
+const ProjectsContainer = () => {
+    return (
+      <section className="projects-container">
+        <h2>Projetos</h2>
+        <p>
+        
+        </p>
+        <a href="https://github.com/Mateusnasciment?tab=repositories" className="btn">
+          Ver Projetos
+        </a>
+      </section>
+    );
+  };
+  
+  export default ProjectsContainer;
